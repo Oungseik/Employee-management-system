c94a00b07c2f5eaaefaedfbf66fa9fe1b8b5df60
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-06-21 20:55:35 +0630

    refactor: add debug log in check health route

--- src/routers/check_health.rs
+++ src/routers/check_health.rs
@@ -1,9 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{response::IntoResponse, routing::get, Router};
+use tracing::debug;
 
 pub fn routes() -> Router {
     Router::new().route("/check-health", get(check_health))
 }
 
-async fn check_health<'a>() -> &'a str {
+async fn check_health<'a>() -> impl IntoResponse {
+    debug!("GET /check-health");
     "server is up and running"
 }
