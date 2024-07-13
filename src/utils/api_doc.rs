68ee97e1fd6c270eba1b52ae219f18ba211f803b
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-13 18:01:40 +0630

    feat: add security addon in doc

--- src/utils/api_doc.rs
+++ src/utils/api_doc.rs
@@ -1,11 +1,28 @@
-use utoipa::OpenApi;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
 use utoipauto::utoipauto;
 
 #[utoipauto]
 #[derive(OpenApi)]
-#[openapi(
-    components(),
-    tags(
-    (name = "Employee management", description = "Service to manage employees")
-))]
+#[openapi( modifiers(&SecurityAddon))]
 pub struct ApiDoc;
+
+pub struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(component) = openapi.components.as_mut() {
+            component.add_security_scheme(
+                "Authorization",
+                SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("Authorization Header"))),
+            );
+
+            component.add_security_scheme(
+                "auth_token",
+                SecurityScheme::ApiKey(ApiKey::Cookie(ApiKeyValue::new("auth_token"))),
+            );
+        }
+    }
+}
